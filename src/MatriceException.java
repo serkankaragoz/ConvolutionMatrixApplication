9485a1796ac59df88a32e481bba7d0bd569c0aa8
serkankaragoz <86783823+serkankaragoz@users.noreply.github.com>
2022-02-27 21:40:42 +0300

    Delete MatriceException.java

--- src/MatriceException.java
+++
@@ -1,8 +0,0 @@
-public class MatriceException extends RuntimeException{
-    public MatriceException(){
-        super();
-    }
-    public MatriceException(String message){
-        super(message);
-    }
-}
